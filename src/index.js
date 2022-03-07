526836b567004b9f8dd91b51c9504fe452efd07a
Bartek O <bartek@fos.nl>
2022-03-07 22:02:44 +0100

    Added navbar to every page

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import reportWebVitals from './reportWebVitals';
 import './css/index.css';
 import App from './App';
 
+import Navbar from './Components/Navbar';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
