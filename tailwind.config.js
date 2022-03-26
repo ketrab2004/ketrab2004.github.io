cc61670e5573950840d888fd833c1054aa1be7b6
Bartek O <bartek@fos.nl>
2022-03-26 18:26:15 +0100

    Added google fonts to tailwind
    with extra backup fonts :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
-        extend: {
-            
-        },
+        fontFamily: {
+            'sans': ['\'Open Sans\'', 'Arial', 'sans-serif'],
+            'nunito': ['\'Nunito\'', 'Verdana', 'sans-serif'],
+            'roboto': ['\'Roboto\'', 'Calibri', 'sans-serif'],
+            'mono': ['\'Red Hat Mono\'', '"Lucida Console"', 'monospace']
+        }
     },
     plugins: [
         
