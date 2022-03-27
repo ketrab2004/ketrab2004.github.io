de6fc46251aa8929ff1c3861e3533cdef65f70e6
Bartek O <bartek@fos.nl>
2022-03-27 18:36:28 +0200

    Created module for re-exporting everything bundled together

---
+++ src/Data/module.ts
@@ -0,0 +1,13 @@
+import Projects from "./Projects";
+import IProject from "./IProject";
+import { default as Tag, Language, Tool, System } from "./Tags";
+
+export default Projects;
+export {
+    Projects,
+    IProject,
+    Tag,
+        Language,
+        Tool,
+        System
+};
