326c4c56fbc7323ad3f33687df564d6a9b8dd096
Bartek O <bartek@fos.nl>
2022-03-27 19:21:22 +0200

    Changed date of cardboard-mod to be the same as on the steam workshop
    just copy and paste, but removing the pm because otherwise it doesn't work

--- src/Data/Projects.ts
+++ src/Data/Projects.ts
@@ -5,7 +5,7 @@ const Projects: {[key: string]: IProject} = {
         highlighted: false,
         title: "The cardboard mod",
         image: "https://steamuserimages-a.akamaihd.net/ugc/939458259771732109/F879436CA1E6EAA94FF9AA7E888006535CB5A341/",
-        date: new Date("21 may 2018 12:23 +0100"),
+        date: new Date("21 May, 2018 @ 12:23"),
         tags: [
             "no-code",
             "blender",
