cde0cbd273c29867f7b81ef98cb40835eabd3628
Bartek O <bartek@fos.nl>
2022-03-27 19:49:38 +0200

    Made project specific page display 404 if project isn't found
    for now it always displays it...

--- src/Pages/ProjectView.tsx
+++ src/Pages/ProjectView.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Error404 from "./404";
 import { useParams } from "react-router-dom"
 
 function ProjectView(): JSX.Element {
     const params = useParams();
     const project = params.project;
 
-    return (
+    const projectExists = false;
+
+    return projectExists ? (
         <main className="ProjectView">
             <h1>Detailed view of a project page</h1>
             <h2>{project}</h2>
         </main>
-    );
+    ) : <Error404 />;
 }
 
 export default ProjectView;
