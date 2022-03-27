bff2baba37ad7710430c65972f0cb01d9eb97664
Bartek O <bartek@fos.nl>
2022-03-27 19:16:50 +0200

    Changed from IProject[] to object<string, IProject>
    and added 1 entry

--- src/Data/Projects.ts
+++ src/Data/Projects.ts
@@ -1,5 +1,17 @@
 import IProject from "./IProject";
 
-const Projects: IProject[] = [];
+const Projects: {[key: string]: IProject} = {
+    "cardboard-mod": {
+        highlighted: false,
+        title: "The cardboard mod",
+        image: "https://steamuserimages-a.akamaihd.net/ugc/939458259771732109/F879436CA1E6EAA94FF9AA7E888006535CB5A341/",
+        date: new Date("21 may 2018 12:23 +0100"),
+        tags: [
+            "no-code",
+            "blender",
+            "paint.net"
+        ]
+    }
+};
 
 export default Projects;
