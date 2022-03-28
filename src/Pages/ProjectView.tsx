ec80daef11013961edfad4a71e88eb426da3e64d
Bartek O <bartek@fos.nl>
2022-03-28 12:48:17 +0200

    Made ProjectView load project data

--- src/Pages/ProjectView.tsx
+++ src/Pages/ProjectView.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Error404 from "./404";
 import { useParams } from "react-router-dom"
+import Projects from "../Data/module";
 
 function ProjectView(): JSX.Element {
     const params = useParams();
-    const project = params.project;
+    const project = Projects[params.project ?? ''];
 
-    const projectExists = false;
+    let {title, thumbnail, date, tags} = project;
 
-    return projectExists ? (
+    return project ? (
         <main className="ProjectView">
-            <h1>Detailed view of a project page</h1>
-            <h2>{project}</h2>
+            <h1>{title}</h1>
+            
         </main>
     ) : <Error404 />;
 }
