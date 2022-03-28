023a92bfc1619ba2ba270452979518b42c5ec630
Bartek O <bartek@fos.nl>
2022-03-28 15:54:22 +0200

    Created StateEnum for keeping track of the state of a project

---
+++ src/Data/StateEnum.ts
@@ -0,0 +1,8 @@
+export const enum StateEnum {
+    Completed = "completed",
+    InProgress = "in progress",
+    OnHold = "on hold",
+    Abandoned = "abandoned"
+}
+
+export default StateEnum;
--- src/Data/module.ts
+++ src/Data/module.ts
@@ -1,11 +1,13 @@
 import Projects from "./Projects";
 import IProject from "./IProject";
+import StateEnum from "./StateEnum";
 import type { default as Tag, Language, Tool, System, Type } from "./Tags";
 
 export default Projects;
 export {
     Projects,
     IProject,
+    StateEnum,
     Tag,
         Language,
         Tool,
