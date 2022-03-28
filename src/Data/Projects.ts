c89876718eae9177363d033b0d21edb0482111fe
Bartek O <bartek@fos.nl>
2022-03-28 16:51:41 +0200

    Added grass-mod to projects
    and added possibility of using %PUBLIC_URL%

--- src/Data/Projects.ts
+++ src/Data/Projects.ts
@@ -1,16 +1,33 @@
+import { default as State } from "./StateEnum";
 import IProject from "./IProject";
+
+// PUBLIC_URL
+const url = window.location.origin;
 
 const Projects: {[key: string]: IProject} = {
     "cardboard-mod": {
         highlighted: false,
-        title: "The cardboard mod",
-        thumbnail: "https://steamuserimages-a.akamaihd.net/ugc/939458259771732109/F879436CA1E6EAA94FF9AA7E888006535CB5A341/",
+        state: State.Abandoned,
+        title: "The Cardboard Mod",
+        thumbnail: `${url}/projects/cardboard-mod/thumbnail.jfif`,
         date: new Date("21 May, 2018 @ 12:23"),
 
         type: "mod",
         languages: ["no-code"],
         tools: ["paint.net", "blender"],
         system: "other",
+    },
+    "grass-mod": {
+        highlighted: false,
+        state: State.Abandoned,
+        title: "The Grass Mod",
+        thumbnail: `${url}/projects/grass-mod/thumbnail.png`,
+        date: new Date("10/01/2017 11:19"), // m d y
+
+        type: "mod",
+        languages: ["no-code"],
+        tools: ["paint.net", "mcreator"],
+        system: "minecraft",
     }
 };
 
