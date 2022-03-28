08c24cc43bdaa33c759f26755611ac5d29d5c69d
Bartek O <bartek@fos.nl>
2022-03-28 20:10:13 +0200

    Created tag component to use for project's tags

---
+++ src/Components/Tag.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import CapitalizeWords from "../functions/CapitalizeWords";
+
+import { Link } from 'react-router-dom';
+import { Language, Tool, System, Type } from "../Data/module";
+
+export function TagItem({name, to}: {name: Language|Tool|System|Type, to?: string}): JSX.Element {
+    const Clickable = to ? Link : "a";
+    return (
+        <li className="inline-block p-1 bg-sky-500 rounded shadow">
+            <Clickable to={to ?? ''}>
+                {CapitalizeWords(name)}
+            </Clickable>
+        </li>
+    );
+}
+
+export function TagsHolder({children, className}: {children: JSX.Element[], className?: string}): JSX.Element {
+    return (
+        <ul className={`flex flex-wrap ${className ?? ''}`}>
+            {children}
+        </ul>
+    );
+}
