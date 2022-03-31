af393c9e1f5c2c8118ebc37215415067478c7dfd
Bartek O <bartek@fos.nl>
2022-03-31 21:02:19 +0200

    Created module files for Components, Functions and Pages

---
+++ src/Pages/module.ts
@@ -0,0 +1,14 @@
+import Home from "./Home";
+import Contact from "./Contact";
+import Projects from "~/Data/Projects/Projects";
+import ProjectView from "./ProjectView";
+import Error404 from "./404";
+
+export {
+    Home,
+    Contact,
+    Projects,
+        ProjectView,
+    
+    Error404
+};
