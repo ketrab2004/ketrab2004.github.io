d4e961b4f1c36b44fc4828867bfaff008c8f804e
Bartek O <bartek@fos.nl>
2022-04-01 14:10:27 +0200

    Added big screen breakpoints to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
             'mono': ['\'Red Hat Mono\'', '"Lucida Console"', 'monospace'],
 
             'comic': ['"Comic Sans MS"', '"Comic Sans"', 'cursive']
+        },
+        screens: {
+            'sm':  '640px',
+            'md':  '768px',
+            'lg':  '1024px',
+            'xl':  '1280px',
+            '2xl': '1536px',
+            '3xl': '1920px',
+            '4xl': '2560px',
         }
     },
     plugins: [
