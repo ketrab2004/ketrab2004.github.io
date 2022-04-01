ec4b30e2a01f331191ae916cebd8faffebf2d1d5
Bartek O <bartek@fos.nl>
2022-04-01 14:11:11 +0200

    Made the gap increase between project items on large screens

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -7,7 +7,7 @@ export function ProjectsPage(): JSX.Element {
         <main className="px-5">
             <h1 className="text-3xl mb-2">Projects:</h1>
             
-            <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div className="grid gap-4 xl:gap-5 2xl:gap-7 3xl:gap-12 4xl:gap-36 content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {/* loop through Projects and render them inside ProjectItems */}
                 {Object.keys(Projects).map(key => <ProjectItem key={key} url={key} project={Projects[key]} />)}
             </div>
