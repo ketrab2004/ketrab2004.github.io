98b9ecdf26d8d12392116e6fded70f0ffa73c09f
Bartek O <bartek@fos.nl>
2022-04-02 17:17:06 +0200

    Imported data/projects

---
+++ data/projects/module.ts
@@ -0,0 +1,10 @@
+import Projects from "./Projects";
+import IProject from "./IProject";
+import StateEnum from "./StateEnum";
+
+export default Projects;
+export {
+    Projects,
+    IProject,
+    StateEnum,
+};
