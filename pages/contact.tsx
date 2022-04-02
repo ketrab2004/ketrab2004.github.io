801c5930f9243c80fe35ae39efd1c7ac27082da4
Bartek O <bartek@fos.nl>
2022-04-02 22:18:59 +0200

    Added 404 and contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Contact(): JSX.Element {
+    return (
+        <main>
+            <h1 className="text-3xl mb-2">Contact me</h1>
+        </main>
+    );
+}
+
+export default Contact;
