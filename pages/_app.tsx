97db50eb70610df44ed4e25bb39b3ee344120b19
Bartek O <bartek@fos.nl>
2022-04-02 17:17:35 +0200

    Added navbar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import * as Components from "@components";
 
 function MyApp({ Component, pageProps }: AppProps) {
     console.log(pageProps, Component.displayName);
     
 
-    return <Component {...pageProps} />
+    return <>
+        <Components.Navbar />
+
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
