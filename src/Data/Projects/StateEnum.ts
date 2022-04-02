99b6deb6a4d4342107b0646edb6c01e56c87964b
Bartek O <bartek@fos.nl>
2022-04-02 16:20:20 +0200

    Moved everything to backup folder

--- src/Data/Projects/StateEnum.ts
+++
@@ -1,9 +0,0 @@
-export const enum StateEnum {
-    ReceivingUpdates = "receiving updates",
-    Completed = "completed",
-    InProgress = "in progress",
-    OnHold = "on hold",
-    Abandoned = "abandoned"
-}
-
-export default StateEnum;
