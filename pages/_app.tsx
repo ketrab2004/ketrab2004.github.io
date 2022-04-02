5f703e66b394de97e5a342fafee90adb231eb620
Bartek O <bartek@fos.nl>
2022-04-02 21:46:11 +0200

    Added google fonts and google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,29 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 
 import * as Components from "@components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    console.log(pageProps, Component.displayName);
+    console.log(pageProps, Component.name);
 
     return <>
         <Components.Navbar />
 
         <Component {...pageProps} />
+
+        {/* Google Analytics - Global site tag (gtag.js) */}
+        <Script id="test" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-9XK6W489R3" />
+        <Script strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: [
+                "window.dataLayer = window.dataLayer || [];",
+                "function gtag(){dataLayer.push(arguments);}",
+                "gtag('js', new Date());",
+
+                "gtag('config', 'G-9XK6W489R3');"
+                ].join("\n")
+            }} />
     </>
 }
 
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+// import Script from "next/script";
+
+class HTMLDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+        <Html>
+            <Head>
+                {/* Google Fonts */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Nunito:wght@400&family=Roboto:wght@300&family=Red+Hat+Mono:wght@400&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+
+                <NextScript  />
+            </body>
+        </Html>
+        )
+    }
+}
+
+export default HTMLDocument
