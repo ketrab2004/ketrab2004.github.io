95b34898d5e67428c4ef770ac42da717b6b02f6b
Bartek O <bartek@fos.nl>
2022-04-03 14:39:01 +0200

    Replaced handwritten path maps for projects with a forEach loop in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,23 @@ const nextConfig = {
     reactStrictMode: true,
 
     exportPathMap: async function(defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+        let projects = {};
+        [
+            "cardboard-mod",
+            "grass-mod",
+            "cannibalism-mod",
+            "html5-hangman",
+            "roblox-infinite-terrain"
+        ].forEach(project => {
+            projects[`projects/${project}`] = { page: "/projects/[project]", query: { project: project } };
+        });
+
         return {
             "/": { page: "/" },
             "contact": { page: "/contact" },
             "projects": { page: "/projects" },
 
-            "projects/cardboard-mod": { page: "/projects/[project]", query: { project: "cardboard-mod" } },
-            "projects/grass-mod": { page: "/projects/[project]", query: { project: "grass-mod" } },
-            "projects/cannibalism": { page: "/projects/[project]", query: { project: "cannibalism" } },
-            "projects/html5-hangman": { page: "/projects/[project]", query: { project: "html5-hangman" } },
-            "projects/roblox-infinite-terrain": { page: "/projects/[project]", query: { project: "roblox-infinite-terrain" } },
+            ...projects
         }
     }
 }
