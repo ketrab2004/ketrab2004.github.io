16c50d0b2d45f3c2d856b6fa3d6e7e436e16466f
Bartek O <bartek@fos.nl>
2022-04-03 18:29:39 +0200

    Created a global path for @styles in the tsconfig

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-// import Script from "next/script";
 
 class HTMLDocument extends Document {
     static async getInitialProps(ctx: any) {
