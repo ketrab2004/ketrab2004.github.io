fab3ab1dde9a95837af9141d2f6be6848e37aaca
Bartek O <bartek@fos.nl>
2022-04-04 19:32:52 +0200

    Removed `image-minimizer-webpack-plugin` plugin
    but not the imageminers that were added seperately

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const withPlugins = require("next-compose-plugins");
 
+const isLocal = process.env.IS_LOCAL ?? false;
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+
+    assetPrefix: (isLocal ? './' : '') // on localhost, we need to use relative paths (only works for paths in the root)
 }
 
 module.exports = withPlugins([
