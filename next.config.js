529eb30ccd33c47f48d1e959a2b5ff1bab3b909e
Bartek O <bartek@fos.nl>
2022-04-04 16:18:00 +0200

    Uninstalled `next-optimized-images`, doesn't work with dynamic urls

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require('next-optimized-images');
+// const optimizedImages = require('next-optimized-images');
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
@@ -8,18 +8,18 @@ const nextConfig = {
 
 module.exports = withPlugins([
 
-    [optimizedImages, {
-        images: {
-            disableStaticImages: true,
+    // [optimizedImages, {
+    //     images: {
+    //         disableStaticImages: true,
 
-            handleImages: ["jpeg", "jpg", "png", "svg", "webp", "gif", "ico"],
+    //         handleImages: ["jpeg", "jpg", "png", "svg", "webp", "gif", "ico"],
 
-            name: "[path][name]_w[width]-h[height]__[md4:hash:base62:16]__.[ext]",
+    //         name: "[path][name]_w[width]-h[height]__[md4:hash:base62:16]__.[ext]",
 
-            publicPath: "'/_next/static/chunks/'" // '/_next/static/chunks/images/' remove the /images
-            // because the [path] is included in the name, and the path always starts with /images
-            // so to avoid /images/images/other/image.jpg
-        }
-    }]
+    //         publicPath: "'/_next/static/chunks/'" // '/_next/static/chunks/images/' remove the /images
+    //         // because the [path] is included in the name, and the path always starts with /images
+    //         // so to avoid /images/images/other/image.jpg
+    //     }
+    // }]
 
 ], nextConfig);
