1b47b89fe6afca358a3fad67119920d938efad32
Bartek O <bartek@fos.nl>
2022-04-07 19:29:15 +0200

    Added SEO to the contact page
    no JsonLd, because it's already in _app

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import type { NextPage } from "next";
 
+import { NextSeo } from "next-seo";
+
 export const Contact: NextPage = () => {
-    return (
+    return <>
+        <NextSeo
+            title="Contact"
+            description="Contact info for Bartek Oskam"
+
+            openGraph={{
+                type: "profile",
+                title: "Bartek Oskam contact page",
+                description: "Contact info for Bartek Oskam",
+                profile: {
+                    firstName: "Bartek",
+                    lastName: "Oskam",
+                    username: "ketrab2004",
+                    gender: "male"
+                }
+            }}
+        />
+        {/* Use the default SocialProfileJsonLd defined in _app */}
+
         <main>
             <h1 className="text-3xl mb-2">Contact me</h1>
         </main>
-    );
+    </>;
 }
 
 export default Contact;
