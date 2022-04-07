386babbd639c21a976bc322961c85378f1e006ac
Bartek O <bartek@fos.nl>
2022-04-07 19:14:21 +0200

    Added default SEO to the app (every page)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,54 @@ import Script from "next/script";
 import type { AppProps } from "next/app";
 
 import * as Components from "@components";
+import { DefaultSeo, SocialProfileJsonLd } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <>
+        <DefaultSeo 
+            titleTemplate="%s | Bartek Oskam portfolio"
+            defaultTitle="Bartek Oskam portfolio"
+            description="A portfolio website for Bartek Oskam"
+
+            robotsProps={{
+                maxSnippet: 128,
+                maxImagePreview: "standard",
+                maxVideoPreview: 15, // seconds
+                unavailableAfter: "2104-04-07T12:00:00+0100",
+            }}
+            additionalLinkTags={[
+                {
+                    rel: 'icon',
+                    href: '/favicon.ico',
+                },
+                {
+                    rel: 'manifest',
+                    href: '/manifest.json'
+                }
+            ]}
+
+            twitter={{
+                cardType: "summary"
+            }}
+            openGraph={{
+                site_name: "Bartek Oskam portfolio",
+                title: "Bartek Oskam portfolio",
+                type: "website",
+                description: "A portfolio website for Bartek Oskam",
+
+            }}
+        />
+        <SocialProfileJsonLd 
+            type="Person"
+            name="Bartek Oskam"
+            url="https://ketrab2004.github.io/"
+            sameAs={[
+                "https://www.linkedin.com/in/BartekOskam/",
+                "https://github.com/ketrab2004"
+            ]}
+            scriptId="social-profile-json-ld"
+        />
+
         <Components.Navbar />
 
         <Component {...pageProps} />
