06df195e0bdcdb215dcfea41fb329cf8337eb8c5
Bartek O <bartek@fos.nl>
2022-04-08 12:16:39 +0200

    Added a postbuild script for next-sitemap, with config

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL ?? '',
+    generateRobotsTxt: true,
+    sitemapSize: 10,
+
+    changefreq: "monthly",
+    
+}
