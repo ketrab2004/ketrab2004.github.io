0b873b9492a02f21bbfc1ae4c8164f5d686c53d3
Bartek O <bartek@fos.nl>
2022-04-11 15:43:24 +0200

    Added search data module
    and added it to tsconfig paths

---
+++ data/search/module.ts
@@ -0,0 +1,11 @@
+import OrderEnum from "./OrderEnum";
+import TagSearchTypeEnum from "./TagSearchTypeEnum";
+import type { default as ISearchInfo, ISpecificSearchInfo } from "./ISearchInfo";
+
+export {
+    OrderEnum,
+    TagSearchTypeEnum,
+
+    ISearchInfo,
+    ISpecificSearchInfo
+}
