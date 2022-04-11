4f079a79ebd97ce9ef8c9d03315ea50364cfd790
Bartek O <bartek@fos.nl>
2022-04-11 15:47:05 +0200

    Added a contextProvider to projects page for searching

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { ReactElement, createContext, useContext } from "react";
 import type { NextPage } from "next";
 
 import { Projects } from "@data/projects";
-import { ProjectItem } from "@components";
+import type { ISearchInfo } from "@data/search";
+import { ProjectItem, Navbar } from "@components";
 import { NextSeo, BreadcrumbJsonLd } from "next-seo";
 import { useState, useEffect } from "react";
 
-export const ProjectsPage: NextPage = () => {
+export const ProjectsPage: NextPage & { getLayout: Function } = () => {
     // declare a stateVariable for the projects
-    const [projects, _setProjects] = useState<typeof Projects>(Projects);
+    // const [projects, _setProjects] = useState<typeof Projects>(Projects);
+    const ctx = useContext(SearchContext);
+    const projects = ctx.projects;
+
+    // console.log(ctx);
+    
 
     const onKeyDown = (e: KeyboardEvent) => {
         if (e.key === "Escape") {
-            console.log("Escape");
+            console.log("Escape", ctx);
         }
     }
 
@@ -46,7 +52,7 @@ export const ProjectsPage: NextPage = () => {
         />
 
         <main className="px-5 pb-5">
-            <h1 className="text-3xl mb-2">Projects:</h1>
+            <h1 className="text-3xl mb-2">Projects: {ctx.search.query}</h1>
             
             <div className="grid gap-4 xl:gap-5 2xl:gap-7 3xl:gap-12 4xl:gap-36 content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {/* loop through Projects and render them inside ProjectItems */}
@@ -58,6 +64,42 @@ export const ProjectsPage: NextPage = () => {
             </div>
         </main>
     </>;
+}
+
+// create context for search data and projects list with default data
+const SearchContext = createContext({projects: Projects, search: {query: "", highlighted: true} as ISearchInfo});
+
+ProjectsPage.getLayout = (page: ReactElement) => {
+    const [search, _setSearch] = useState<ISearchInfo>(useContext(SearchContext).search);
+
+    function applySearch(): typeof Projects {
+        let toReturn = {...Projects}; // copy (not deep), so delete doesn't change Projects
+
+        if (search.query == "bb") delete toReturn["cardboard-mod"];
+
+        return toReturn;
+    }
+
+    function doSearch(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // prevent submitting form
+    
+        _setSearch({...search, query: "bb"});
+
+        console.log(event);
+        console.log(event.target);
+    }
+
+    return (
+        <SearchContext.Provider value={{projects: applySearch(), search}}>
+            <Navbar>
+                <form className="justify-center" onSubmit={doSearch}>
+                    <input name="search" />
+                </form>
+            </Navbar>
+            {page}
+            {/* footer */}
+        </SearchContext.Provider>
+    );
 }
 
 export default ProjectsPage;
