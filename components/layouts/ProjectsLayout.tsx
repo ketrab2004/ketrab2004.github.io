a12d4cfd4a138dbfc360f6b772c604f7280b6ef4
Bartek O <bartek@fos.nl>
2022-04-13 15:14:45 +0200

    Moved the projects layout into the layouts module

---
+++ components/layouts/ProjectsLayout.tsx
@@ -0,0 +1,42 @@
+import { ReactElement, useState } from "react";
+import { Navbar } from "@components";
+import { SearchContext, useSearchContext } from "@context";
+import Projects from "@data/projects";
+import { ISearchInfo } from "@data/search";
+
+export default function ProjectsLayout(page: ReactElement) {
+    const [search, _setSearch] = useState<ISearchInfo>(useSearchContext().search);
+
+    function applySearch(): typeof Projects {
+        let toReturn = {...Projects}; // copy (not deep), so delete doesn't change Projects
+
+        if (search.query == "bb") delete toReturn["cardboard-mod"];
+
+        return toReturn;
+    }
+
+    function doSearch(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // prevent submitting form
+    
+        _setSearch({...search, query: "bb"});
+
+        console.log(event);
+        console.log(event.target);
+    }
+
+    return (
+        <SearchContext.Provider value={{projects: applySearch(), search}}>
+            <Navbar>
+                <form className="flex items-center font-nunito text-sm ml-4" onSubmit={doSearch}>
+                    <input name="search" className="rounded-md border-black border p-1 mr-2" />
+                    <button className="
+                        rounded-md bg-sky-600 px-3 py-2 transition-colors text-white
+                        hover:bg-blue-700
+                        ring-blue-600 ring-opacity-25 active:ring-2" type="button">Filters</button>
+                </form>
+            </Navbar>
+            {page}
+            {/* footer */}
+        </SearchContext.Provider>
+    );
+}
