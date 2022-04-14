13c049fbfb16a2c00eeb6af1ed50ee4299013c4c
Bartek O <bartek@fos.nl>
2022-04-14 12:11:11 +0200

    Made ISearchHolder use SCProject, so we can convert between the project list and a search temp list easily

--- data/search/ISearchHolder.ts
+++ data/search/ISearchHolder.ts
@@ -1,9 +1,7 @@
-import OrderEnum from "./OrderEnum";
-import TagSearchTypeEnum from "./TagSearchTypeEnum";
-import { IProject } from "@data/projects";
+import { SCProject } from "@context"
 
 export interface ISearchHolder {
-    projects: IProject, // the item that is being sorted/searched
+    project: SCProject, // the item that is being sorted/searched
 
     relevance?: number, // the relevance of the item, used for sorting
 }
