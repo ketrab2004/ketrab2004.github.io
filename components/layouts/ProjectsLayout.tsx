027c691517a2583898285843baf68fbbced785ad
Bartek O <bartek@fos.nl>
2022-04-15 17:31:43 +0200

    Added interfaces so we can read the form info in typescript in ProjectsLayout
    and added using those values to change the search info

--- components/layouts/ProjectsLayout.tsx
+++ components/layouts/ProjectsLayout.tsx
@@ -3,21 +3,44 @@ import { Navbar } from "@components";
 import { SearchContext, useSearchContext, getSCProjects } from "@context";
 import { ISearchInfo, OrderEnum } from "@data/search";
 import { applySearch } from "@functions";
-import { format as formatDate } from "date-fns";
+
+
+// https://stackoverflow.com/a/68253165
+interface IFormElementCollection extends HTMLFormControlsCollection {
+    search: HTMLInputElement,
+    highlightedOnly: HTMLInputElement,
+
+    orderBy: HTMLSelectElement,
+    orderDir: HTMLSelectElement,
+
+    afterDate: HTMLInputElement,
+    beforeDate: HTMLInputElement,
+}
+interface IFormInfo extends HTMLFormElement { readonly elements: IFormElementCollection }
+
 
 export default function ProjectsLayout(page: ReactElement) {
     const searchContext = useSearchContext();
     const [search, _setSearch] = useState<ISearchInfo>(searchContext.search);
 
-    const doSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    const doSearch = (event: React.FormEvent<IFormInfo>) => {
         event.preventDefault(); // prevent submitting form
 
-        const eventForm = event.target as HTMLElement; // cast to HTMLElement, so you can use querySelector
+        const elements = event.currentTarget.elements;
 
         _setSearch({...search, // keep old search info
-            query: (eventForm.querySelector("input[name=search]") as HTMLInputElement) // cast to HTMLInputElement so you can use value
-                    .value ?? '' // value or empty string
+            query: elements.search.value ?? search.query,
+            highlighted: elements.highlightedOnly.checked ?? search.highlighted,
+
+            order: elements.orderBy.value as OrderEnum ?? search.order,
+            orderAsc: elements.orderDir.value == "true" ?? search.orderAsc,
+
+            afterDate: elements.afterDate.valueAsDate ?? undefined,
+            beforeDate: elements.beforeDate.valueAsDate ?? undefined
         });
+
+        console.log(search);
+        
     }
 
     // toggle the filter element
@@ -44,21 +67,21 @@ export default function ProjectsLayout(page: ReactElement) {
                             bg-white shadow-md">
 
                             <div className="pb-2"> {/* show highlighted projects only */}
-                                <input type="checkbox" id="highlighted-only" name="highlighted-only" defaultChecked />
-                                <label htmlFor="highlighted-only" className="pl-2 select-none">Show highlighted only</label>
+                                <input type="checkbox" id="highlightedOnly" name="highlightedOnly" defaultChecked />
+                                <label htmlFor="highlightedOnly" className="pl-2 select-none">Show highlighted only</label>
                             </div>
 
                             {/* ordering */}
                             <div className="flex flex-row pb-2">
                                 <div className="flex flex-col justify-start"> {/* choose order by */}
-                                    <label htmlFor="order-by">Order by</label>
-                                    <select className="border rounded" id="order-by" name="order-by">
+                                    <label htmlFor="orderBy">Order by</label>
+                                    <select className="border rounded" id="orderBy" name="orderBy">
                                         {Object.keys(OrderEnum).map(key => <option value={key}>{key.toLowerCase()}</option>)}
                                     </select>
                                 </div>
                                 <div className="flex flex-col justify-start ml-4"> {/* choose order by direction */}
-                                    <label htmlFor="is-asc">Direction</label>
-                                    <select className="border rounded" id="is-asc" name="is-asc">
+                                    <label htmlFor="orderDir">Direction</label>
+                                    <select className="border rounded" id="orderDir" name="orderDir">
                                         <option value="false">Descending</option>
                                         <option value="true">Ascending</option>
                                     </select>
@@ -68,14 +91,14 @@ export default function ProjectsLayout(page: ReactElement) {
                             {/* date filtering */}
                             <div className="flex flex-row justify-between pb-2">
                                 <div className="flex flex-col justify-start">
-                                    <label htmlFor="date-after">After</label>
-                                    <input type="date" id="date-after" name="date-after" className="border rounded max-w-min"
+                                    <label htmlFor="afterDate">After</label>
+                                    <input type="date" id="afterDate" name="afterDate" className="border rounded max-w-min"
                                         min={new Date("2016").toLocaleDateString("en-ca") /* https://stackoverflow.com/a/49916376 */}
                                         max={new Date().toLocaleDateString("en-ca")}/>
                                 </div>
                                 <div className="flex flex-col justify-start">
-                                    <label htmlFor="date-before">Before</label>
-                                    <input type="date" id="date-before" name="date-before" className="border rounded max-w-min"
+                                    <label htmlFor="beforeDate">Before</label>
+                                    <input type="date" id="beforeDate" name="beforeDate" className="border rounded max-w-min"
                                         min={new Date("2016").toLocaleDateString("en-ca")}
                                         max={new Date().toLocaleDateString("en-ca")} />
                                 </div>
