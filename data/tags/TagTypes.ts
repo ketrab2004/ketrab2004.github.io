12b6d689ebfc9a42010c35d45eddccc8ca92381d
Bartek O <bartek@fos.nl>
2022-04-15 20:10:42 +0200

    Converted TagTypes into const arrays, with types made from them
    so i can loop through all the types :)

--- data/tags/TagTypes.ts
+++ data/tags/TagTypes.ts
@@ -1,60 +1,69 @@
 export type Tag = Language | Tool | System | Type;
 export default Tag;
 
-export type Language =
-    "c" |
-    "c++" |
-    "c#" |
-    "lua" |
-    "luau" |
-    "js" |
-    "ts" |
-    "md" |
-    "hlsl" |
-    "java" |
-    "kotlin" |
-    "dart" |
-    "mysql" |
-    "sql" |
-    "html" |
-    "css" |
-    "scss" |
-    "php" |
-    "no-code";
+// https://stackoverflow.com/a/64174790
 
-export type Tool =
-    "vscode" |
-    "visual studio" |
-    "roblox studio" |
-    "mcreator" |
-    "sublime text" |
-    "blender" |
-    "paint.net" |
-    "sourcetree" |
-    "git gui" |
-    "git";
+export const Languages = [
+    "c",
+    "c++",
+    "c#",
+    "lua",
+    "luau",
+    "js",
+    "ts",
+    "md",
+    "hlsl",
+    "java",
+    "kotlin",
+    "dart",
+    "mysql",
+    "sql",
+    "html",
+    "css",
+    "scss",
+    "php",
+    "no-code"
+] as const;
+export type Language = typeof Languages[number];
 
-    
-    export type Type =
-    "game" |
-    "plugin" |
-    "library" |
-    "mod" |
-    "app" |
-    "website" |
-    "other";
+export const Tools = [
+    "vscode",
+    "visual studio",
+    "roblox studio",
+    "mcreator",
+    "sublime text",
+    "blender",
+    "paint.net",
+    "sourcetree",
+    "git gui",
+    "git"
+] as const;
+export type Tool = typeof Tools[number];
 
-export type System =
-    "unity" |
-    "unreal engine" |
-    "platformio" |
-    "roblox" |
-    "minecraft" |
-    "other (ingame)" |
-    "flutter" |
-    "spigot" |
-    "forge" |
-    "laravel" |
-    "react" |
-    "php" |
-    "raw html";
+export const Types = [
+    "game",
+    "plugin",
+    "library",
+    "mod",
+    "app",
+    "website",
+    "other"
+] as const;
+export type Type = typeof Types[number];
+
+export const Systems = [
+    "unity",
+    "unreal engine",
+    "platformio",
+    "roblox",
+    "minecraft",
+    "other (ingame)",
+    "flutter",
+    "spigot",
+    "forge",
+    "laravel",
+    "react",
+    "php",
+    "raw html"
+] as const;
+export type System = typeof Systems[number];
