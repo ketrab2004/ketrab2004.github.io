6926f9831134b5731e8344f8d60f052e934c82d7
Bartek O <bartek@fos.nl>
2022-04-16 14:51:42 +0200

    Added filtering by date functionality to the filterSearchHolders function
    i forgot :(

--- functions/filterSearchHolders.ts
+++ functions/filterSearchHolders.ts
@@ -1,5 +1,6 @@
 import { ISearchHolder, ISearchInfo, TagSearchTypeEnum } from "@data/search";
 import { Tag } from "@data/tags";
+import { isAfter, isBefore } from "date-fns";
 
 export function matchesTags(tags: Tag[], filterTags: Tag[], mode: TagSearchTypeEnum): boolean {
     if (filterTags.length <= 0) return true; // no tags given, so it should match
@@ -32,23 +33,31 @@ export function filterSearchHolders(projects: ISearchHolder[], search: ISearchIn
         // check if it matches the highlightedOnly option
         // if (search.highlighted && !(projectHolder.project.highlighted ?? false)) return; //TODO uncomment this, when highlighted projects are added
 
+        // check if it's after the afterDate if it's set
+        if (search.afterDate !== undefined && isBefore(projectHolder.project.date, search.afterDate))
+            return; // if it's before the afterDate, return (don't add it to toReturn)
+
+        // check if it's before the beforeDate if it's set
+        if (search.beforeDate !== undefined && isAfter(projectHolder.project.date, search.beforeDate))
+            return; // if it's after the beforeDate, return (don't add it to toReturn)
+
         // check if it matches the project type tag
         // TagSearchTypeEnum is OR by default, because a project can only have 1 type
         if (!matchesTags([projectHolder.project.type], search.type.tags, TagSearchTypeEnum.OR))
-            return; // if it doesn't match, return (and don't add it to toReturn)
+            return; // if it doesn't match, return (don't add it to toReturn)
 
         // check if it matches the project system tag
         // TagSearchTypeEnum is OR by default, because a project can only have 1 system
         if (!matchesTags([projectHolder.project.system], search.system.tags, TagSearchTypeEnum.OR))
-            return; // if it doesn't match, return (and don't add it to toReturn)
+            return; // if it doesn't match, return (don't add it to toReturn)
 
         // check if it matches the languages tags
         if (!matchesTags(projectHolder.project.languages, search.languages.tags, search.languages.mode))
-            return; // if it doesn't match, return (and don't add it to toReturn)
+            return; // if it doesn't match, return (don't add it to toReturn)
 
         // check if it matches the tools tags
         if (!matchesTags(projectHolder.project.tools, search.tools.tags, search.tools.mode))
-            return; // if it doesn't match, return (and don't add it to toReturn)
+            return; // if it doesn't match, return (don't add it to toReturn)
 
         // if all checks passed, add it to toReturn
         toReturn.push(projectHolder);
