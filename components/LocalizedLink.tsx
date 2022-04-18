3f2f8d8864b4e3bafde3a9e7425f9c7414292b3d
Bartek O <bartek@fos.nl>
2022-04-18 18:00:45 +0200

    Fixed the LocalizedLink component
    not redirecting to localized links

--- components/LocalizedLink.tsx
+++ components/LocalizedLink.tsx
@@ -3,27 +3,26 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export interface ILocalizedLinkProps {
-    children: JSX.Element[],
     skipLocaleHandling?: boolean,
     href: string,
     locale?: string
 }
 
 // https://locize.com/blog/next-i18n-static/
-export function LocalizedLink({ children, skipLocaleHandling, href, locale, ...rest }: ILocalizedLinkProps) {
+export function LocalizedLink({ children, skipLocaleHandling = false, href, locale, ...rest }: React.PropsWithChildren<ILocalizedLinkProps>) {
     const router = useRouter()
     const l = locale || router.query.locale as string || '';
 
     let h = href || router.asPath;
     if (h.indexOf('http') === 0) skipLocaleHandling = true;
-    if (locale && !skipLocaleHandling) {
+    if (l && !skipLocaleHandling) {
         h = h
-            ? `/${locale}${h}`
+            ? `/${l}${h}`
             : router.pathname.replace('[locale]', l);
     }
 
     return (
-        <Link href={href} {...rest}>
+        <Link href={h} {...rest}>
             {children}
         </Link>
     );
