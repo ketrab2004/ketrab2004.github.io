aa175fead8f2878a247703b346e4d8e643bcc0d1
Bartek O <bartek@fos.nl>
2022-04-18 16:44:26 +0200

    Commented out isLocal assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isLocal = process.env.IS_LOCAL ?? false;
+// const isLocal = process.env.IS_LOCAL ?? false;
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    assetPrefix: (isLocal ? "./" : ''), // on localhost, we need to use relative paths (only works for paths in the root)
+    // assetPrefix: (isLocal ? "./" : ''), // on localhost, we need to use relative paths (only works for paths in the root)
     
     reactStrictMode: true
 }
