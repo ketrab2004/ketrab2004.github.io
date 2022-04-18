b311340d888023a8cdef7c5e760d4a1172996171
Bartek O <bartek@fos.nl>
2022-04-18 15:38:25 +0200

    Flipped the settings in next.config.js for easier editing
    because the long comment on assetPrefix makes it hard to add/remove the comma

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const isLocal = process.env.IS_LOCAL ?? false;
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-
-    assetPrefix: (isLocal ? "./" : '') // on localhost, we need to use relative paths (only works for paths in the root)
+    assetPrefix: (isLocal ? "./" : ''), // on localhost, we need to use relative paths (only works for paths in the root)
+    
+    reactStrictMode: true
 }
 
 module.exports = nextConfig;
